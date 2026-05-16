487b4f2c98f5a8564f2426851880de661f8d7e26
Etikala Srinivas | Data Science Student <127411703+srinivas060@users.noreply.github.com>
2026-05-16 16:13:38 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -206,4 +206,5 @@ def update_task(id):
 def home():
     return "Team Task Manager backend running Sucessfully "
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
