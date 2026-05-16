c2c93d453dd44798b328179dbec686bc34621124
Etikala Srinivas | Data Science Student <127411703+srinivas060@users.noreply.github.com>
2026-05-16 15:32:23 +0530

    Update app.run to use environment PORT variable

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_jwt_extended import (
     JWTManager,
@@ -201,5 +202,7 @@ def update_task(id):
     db.session.commit()
 
     return jsonify({'message':'Task updated'})
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
